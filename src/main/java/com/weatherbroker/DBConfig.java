7f277404bbe9ecc45adc73d483abde0ca51602f2
virus29 <imya29>
2018-04-06 18:20:21 +0300

    1

--- src/main/java/com/weatherbroker/DBConfig.java
+++ src/main/java/com/weatherbroker/DBConfig.java
@@ -1,31 +1,22 @@
 package com.weatherbroker;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 import javax.naming.NamingException;
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories
-@ComponentScan({"com.weatherbroker"})
-//@EnableTransactionManagement
 public class DBConfig {
 
-    @Autowired
-    private Environment env;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory()
@@ -34,7 +25,7 @@ public class DBConfig {
                 = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource());
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+//        properties.setProperty("hibernate.hbm2ddl.auto", "create");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.setProperty("hibernate.current_session_context_class", "thread");
@@ -44,14 +35,14 @@ public class DBConfig {
     }
 
     @Bean
-    public DataSource dataSource() throws NamingException {
-        return (DataSource) new JndiTemplate().lookup("java:/MySqlDS");
+    public DriverManagerDataSource dataSource() throws NamingException {
+        return (DriverManagerDataSource) new JndiTemplate().lookup("java:/MySqlDS");
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+    JpaTransactionManager transactionManager() throws NamingException {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
         return transactionManager;
     }
 
